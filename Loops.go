d087892cb6be2cc74ab66b2ebddefaffa6ac2abe
isha-dhek <isha.dhek02@gmail.com>
2022-01-08 11:44:54 +0530

    Loops, Its uses and its control statements

---
+++ Loops.go
@@ -0,0 +1,45 @@
+//contains only a single loop that is for-loop
+//As simple for loop
+// Go program to illustrate the
+// use of simple for loop
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 7; i++ {
+		fmt.Printf("Hello World!\n")
+	}
+
+}
+
+//For loop as Infinite Loop
+// Go program to illustrate the
+// use of an infinite loop
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	// Infinite loop
+// 	for {
+// 		fmt.Printf("Hello World!\n")
+// 	}
+
+// }
+// More uses of For loop:
+//for loop as while Loop
+// Simple range in for loop
+// Using for loop for strings
+// For Maps
+//For Channel
+// Important Points:
+// Parentheses are not used around the three statements of a for loop.
+// If the array, string, slice, or map is nil then the number of iterations of the for loop is zero.
+//Go language supports 3 types of loop control statements:
+
+//1.Break
+//2.Goto
+//3.Continue
